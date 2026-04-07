67385d2e83ea41272249dedc1aaacf00c95f8b07
Oscar Munoz <oscarxxi@gmail.com>
2026-04-06 22:19:57 -0500

    adding register button in login page

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -63,6 +63,10 @@ new #[Layout('layouts.guest')] class extends Component
                 </a>
             @endif
 
+            <a href="{{ route('register') }}" class="ms-4 inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150" wire:navigate>
+                {{ __('Register') }}
+            </a>
+
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>

a85bf3bb72c470d6f4614f087de27c03212abe18
Oscar Munoz <oscarxxi@gmail.com>
2026-04-11 14:08:32 -0500

    changing the way of showing the admin options manage

--- resources/views/livewire/admin/navigation.blade.php
+++ resources/views/livewire/admin/navigation.blade.php
@@ -29,16 +29,43 @@ new class extends Component
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex items-center">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </x-nav-link>
                     <x-nav-link :href="route('leaderboard')" :active="request()->routeIs('leaderboard')" wire:navigate>
                         {{ __('Leaderboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('admin.users')" :active="request()->routeIs('admin.users')" wire:navigate>
-                        {{ __('Manage Admins') }}
-                    </x-nav-link>
+
+                    <!-- Manage Dropdown -->
+                    <div class="hidden sm:flex sm:items-center h-16">
+                        <x-dropdown align="left" width="48" contentClasses="py-1 bg-white">
+                            <x-slot name="trigger">
+                                @php
+                                    $manageActive = request()->routeIs('admin.*');
+                                    $manageClasses = $manageActive
+                                        ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out cursor-pointer h-full'
+                                        : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out cursor-pointer h-full';
+                                @endphp
+                                <button class="{{ $manageClasses }}">
+                                    <div>{{ __('Manage') }}</div>
+
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('admin.users')" wire:navigate>
+                                    {{ __('Admins') }}
+                                </x-dropdown-link>
+                                <!-- Future manage links here -->
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
                 </div>
             </div>
 
@@ -93,9 +120,19 @@ new class extends Component
             <x-responsive-nav-link :href="route('leaderboard')" :active="request()->routeIs('leaderboard')" wire:navigate>
                 {{ __('Leaderboard') }}
             </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('admin.users')" :active="request()->routeIs('admin.users')" wire:navigate>
-                {{ __('Manage Admins') }}
-            </x-responsive-nav-link>
+
+            <!-- Mobile Manage Group -->
+            <div class="pt-2 pb-2">
+                <div class="block px-4 py-2 text-xs font-semibold text-gray-500 uppercase tracking-widest bg-gray-50 border-l-4 border-transparent">
+                    {{ __('Manage') }}
+                </div>
+                <div class="mt-1 space-y-1">
+                    <x-responsive-nav-link :href="route('admin.users')" :active="request()->routeIs('admin.users')" class="pl-8" wire:navigate>
+                        {{ __('Admins') }}
+                    </x-responsive-nav-link>
+                    <!-- Future Manage Links go here -->
+                </div>
+            </div>
         </div>
 
         <!-- Responsive Settings Options -->

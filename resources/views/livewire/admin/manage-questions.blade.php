c36c271192c84cfd7630ded214090fa0836575a1
Oscar Munoz <oscarxxi@gmail.com>
2026-04-13 21:21:30 -0500

    making the table use full width

--- resources/views/livewire/admin/manage-questions.blade.php
+++ resources/views/livewire/admin/manage-questions.blade.php
@@ -24,7 +24,7 @@
 
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="overflow-x-auto">
-                        <table class="min-w-full divide-y divide-gray-200">
+                        <table class="w-full divide-y divide-gray-200">
                             <thead class="bg-gray-50">
                                 <tr>
                                     <th scope="col" class="px-6 py-3 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider">Question</th>

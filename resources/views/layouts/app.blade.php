157daaa6b9b1d97d47045e68e9ff16ff0e98abb2
Oscar Munoz <oscarxxi@gmail.com>
2026-04-23 20:55:16 -0500

    adding breadcrumbs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,13 +23,17 @@
             @endif
 
             <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+                    <x-breadcrumbs />
+                    
+                    @if (isset($header))
+                        <div class="mt-2">
+                            {{ $header }}
+                        </div>
+                    @endif
+                </div>
+            </header>
 
             <!-- Page Content -->
             <main>
